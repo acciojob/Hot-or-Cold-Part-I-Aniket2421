2cd2f5b584d7fc29dd42f152ca645277a768171d
Aniket2421 <115690076+Aniket2421@users.noreply.github.com>
2023-03-03 15:40:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,14 @@
-let secretNumber = Math.floor(Math.random() * 100) + 1;
-let prevDifference = 100; // initial value larger than maximum possible difference
-let guessCount = 0;
+const randomNumber = Math.floor(Math.random() * 100) + 1; // generates a random number between 1 and 100
+const output = document.getElementById("respond");
+const guessInput = document.getElementById("guess");
 
 function checkGuess() {
-  const guess = Number(document.getElementById("guess").value);
-  if (guess && guess >= 1 && guess <= 100) { // check if input is valid
-    guessCount++;
-    let difference = Math.abs(guess - secretNumber);
-    let message = "";
-    if (guess === secretNumber) {
-      message = `Congratulations! You guessed the number in ${guessCount} tries.`;
-    } else if (guessCount === 1) {
-      message = `Wrong guess. Try again.`
-    } else if (difference < prevDifference) {
-      message = `Getting hotter. Guess ${guess > secretNumber ? "lower" : "higher"}.`;
-    } else {
-      message = `Getting colder. Guess ${guess > secretNumber ? "higher" : "lower"}.`;
-    }
-    prevDifference = difference;
-    document.getElementById("response").textContent = message;
+  const userGuess = parseInt(guessInput.value);
+  if (userGuess === randomNumber) {
+    output.innerHTML = "Congratulations! You guessed the correct number!";
+  } else if (userGuess > randomNumber) {
+    output.innerHTML = "Hotter";
   } else {
-    document.getElementById("response").textContent = "Please enter a valid number between 1 and 100.";
+    output.innerHTML = "Colder!";
   }
 }
-
-
